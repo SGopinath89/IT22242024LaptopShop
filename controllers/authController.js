8210b2824839ed57da41245e041618bee3c9ecb5
lasanga890 <lasangad890@gmail.com>
2024-05-01 21:31:54 +0530

    day-01-3rd submit
    Create registration backend.its working successfully

---
+++ controllers/authController.js
@@ -0,0 +1,53 @@
+import userModel from "../models/userModel.js";
+
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address, answer } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone no is Required" });
+    }
+    if (!address) {
+      return res.send({ message: "Address is Required" });
+    }
+    //check user
+    const exisitingUser = await userModel.findOne({ email });
+    //exisiting user
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already Register please login",
+      });
+    }
+    //register user
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in registration",
+      error,
+    });
+  }
+};
