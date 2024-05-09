02ec44fccccb4f4943e092336f35707d1c401584
lasanga890 <lasangad890@gmail.com>
2024-05-09 19:42:29 +0530

    start frontend
    create simple UI for website ..navbar and footer and someother things

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
+import { comparePassword, hashPassword } from "../helpers/authHelp.js";
 import userModel from "../models/userModel.js";
 import JWT from "jsonwebtoken";
-
 
 export const registerController = async (req, res) => {
   try {
@@ -31,12 +31,13 @@ export const registerController = async (req, res) => {
       });
     }
     //register user
+    const hashedPassword = await hashPassword(password);
     const user = await new userModel({
       name,
       email,
       phone,
       address,
-      password,
+      password: hashedPassword,
     }).save();
 
     res.status(201).send({
@@ -55,53 +56,62 @@ export const registerController = async (req, res) => {
 };
 
 //POST LOGIN
-export const loginController = async (req,res) => {
-  try{
-    const {email,password} = req.body
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
     //validation
-    if(!email || !password){
+    if (!email || !password) {
       return res.status(404).send({
-        success:false,
-        message:'Invalid email or password'
-      })
+        success: false,
+        message: "Invalid email or password",
+      });
     }
-  //check user
-  const user = await userModel.findOne({email})
-  if(!user){
-    return res.status(404).send({
-      success:false,
-      message:'Email is not registerd'
-    })
-  }
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registerd",
+      });
+    }
 
-  if(password!=user.password){
-    return res.status(404).send({
-      success:false,
-      message:"invalid Password"
-    })
-  }
-
-  //token
-  const token = await JWT.sign({_id:user._id}, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-  res.status(200).send({
-    success:true,
-    message:'login succesfully',
-    user:{
-      name:user.name,
-      email:user.email,
-      phone: user.phone,
-      address: user.address,
-    },
-    token,
-  });
-  }catch(error) {
-    console.log(error)
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "login succesfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
     res.status(500).send({
-      success:false,
-      message:'Error in login',
-      error
-    })
+      success: false,
+      message: "Error in login",
+      error,
+    });
+  }
+};
+
+//test controller
+export const testController = (req, res) => {
+  try {
+    res.send("Protected Routes");
+  } catch (error) {
+    res.send({ error });
   }
 };
