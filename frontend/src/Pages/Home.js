32d21b2bd4dedab87d5b70d282f379f2670b858f
Chesintha <139671816+Chesintha@users.noreply.github.com>
2024-05-13 21:40:32 +0530

    create logout context
    create logout context

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Layout from "../Components/Layout/Layout";
+import { useAuth } from "../Context/auth";
 
 const Home = () => {
+  const [auth,setAuth] = useAuth()
+  console.log("Auth object:", auth);
   return (
     <Layout>
       <h1>Home Page</h1>
+      <pre>
+    {JSON.stringify(auth,null,4)}
+      </pre>
     </Layout>
   );
 };
